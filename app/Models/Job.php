30b4adb2793222c1270d9c973ba603fa0b446b1d
piotr <trawa8231@gmail.com>
2026-01-06 00:20:09 +0100

    After lesson 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use App\Models\Employer;
 
 class Job extends Model
 {
+    use HasFactory;
     protected $table = 'job_listings';
     protected $fillable = ['title', 'salary'];
+
+    public function employer(){
+        return $this->belongsTo(Employer::class);
+    }
 }
